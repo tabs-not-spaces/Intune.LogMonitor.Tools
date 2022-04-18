1e47290e8c2e826d6c54461b57d383dcb694c766
Ben Reader <ben.reader@gmail.com>
2022-04-18 17:32:11 +1000

    new classes built out

--- Intune.Application.Tools/Classes/IntuneApp.psm1
+++ Intune.Application.Tools/Classes/IntuneApp.psm1
@@ -1,13 +1,29 @@
 class IntuneApp {
+    $ApplicationId
     $Url
     $Key
     $IV
     $BinFileName
 
-    IntuneApp([PSCustomObject]$contentInfo, [PSCustomObject]$decryptInfo) {
+    IntuneApp([string]$ApplicationId, [PSCustomObject]$contentInfo, [PSCustomObject]$decryptInfo) {
+        $this.ApplicationId = $applicationId
         $this.Url = $contentInfo.UploadLocation
         $this.Key = $decryptInfo.EncryptionKey
         $this.IV = $decryptInfo.IV
         $this.BinFileName = $(Split-Path $this.Url -Leaf)
+    }
+}
+
+class IntuneApps {
+    [System.Collections.ArrayList]$applications
+    IntuneApps() {
+        $this.applications = [System.Collections.ArrayList]::new()
+    }
+    Add([string]$applicationId, [PSCustomObject]$contentInfo, [PSCustomObject]$decryptInfo) {
+        if ($applicationId -notin $this.applications.ApplicationId) {
+            $intuneApp = [IntuneApp]::new($applicationId, $contentInfo, $decryptInfo)
+            $this.applications.Add($intuneApp)
+            New-Event -SourceIdentifier 'Intune.Application.Tools' -MessageData $intuneApp
+        }
     }
 }
