f794ea878f831deb9f8fc32a6f72e0c00de9a795
Ben Reader <ben.reader@gmail.com>
2022-04-25 00:52:22 +1000

    small refactor

--- Intune.Application.Tools/Public/Start-IMEAppMonitor.ps1
+++ Intune.Application.Tools/Public/Start-IMEAppMonitor.ps1
@@ -5,7 +5,7 @@ function Start-IMEAppMonitor {
         [System.Io.FileInfo]$IMEAgentLogFile = $(Join-Path $env:ProgramData "Microsoft\IntuneManagementExtension\Logs\IntuneManagementExtension.log"),
 
         [parameter(Mandatory = $false)]
-        [System.IO.FileInfo]$OutputFolder = "D:\temp"
+        [System.IO.FileInfo]$OutputFolder = "$env:temp"
     )
 
     try {
@@ -16,29 +16,14 @@ function Start-IMEAppMonitor {
         Write-Host "Monitoring IME log for applications.." -ForegroundColor Yellow
         Write-Host "Press CTRL+C to cancel.." -ForegroundColor Black -BackgroundColor Yellow
 
-        while ($true) {
-            $logData = Get-Content $IMEAgentLogFile
-            $filteredLog = $logData |
-            Select-String -Pattern '\<\!\[LOG\[Response from Intune = {' -AllMatches
-            if ($filteredLog) {
-                foreach ($log in $filteredLog) {
-                    $reply = "{$($logData[$log.LineNumber].ToString().TrimStart())}" | ConvertFrom-Json
-                    if ($reply.ResponseContentType -eq 'GetContentInfo') { 
-                        $responsePayload = $reply.ResponsePayload | ConvertFrom-Json
-                        $contentInfo = $responsePayload.ContentInfo | ConvertFrom-Json
-                        $decryptInfo = ConvertFrom-EncryptedBase64 -B64String ([xml]$responsePayload.DecryptInfo).EncryptedMessage.EncryptedContent | ConvertFrom-Json
-                        $intuneApplications.Add($responsePayload.ApplicationId, $contentInfo, $decryptInfo, $OutputFolder)
-                    }
-                }
-            }
-        }
+        Find-AppDataFromLog -IMEAgentLogFile $IMEAgentLogFile -OutputFolder $OutputFolder
     }
     catch {
         Write-Warning $_.Exception.Message
     }
     Finally {
-        #Set-IMELogLevel -LogLevel "Information"
-        Write-Verbose "This is where we will return the loglevel back to info.."
+        Write-Verbose "Shutting everything down.."
+        Set-IMELogLevel -LogLevel "Information"
         Get-EventSubscriber -SourceIdentifier "Intune.Application.Tools" | Unregister-Event
         Get-Job -Name "Intune.Application.Tools" | Remove-Job
     }
